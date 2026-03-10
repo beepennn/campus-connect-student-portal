093c4db3cc284298edffc7504a3692f5cdbe6d38
beepennn <bipinlamsal11@gmail.com>
2026-03-10 18:42:20 +0545

    Remove emoji from announcements title

--- components/announcements-list.tsx
+++ components/announcements-list.tsx
@@ -34,7 +34,7 @@ export default function AnnouncementsList() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>📝 Announcements</CardTitle>
+        <CardTitle> Announcements</CardTitle>
         <CardDescription>Quick updates</CardDescription>
       </CardHeader>
       <CardContent>
